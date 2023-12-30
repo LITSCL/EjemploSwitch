3320b21a471f45373547c968eaf6e0d273da6a74
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-30 11:47:52 -0300

    Ok

--- app/src/main/java/cl/inacap/ejemploswitch/MainActivity.java
+++ app/src/main/java/cl/inacap/ejemploswitch/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Switch opcionSw;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
